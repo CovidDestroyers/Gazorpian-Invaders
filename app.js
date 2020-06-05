76018d15b4509505b990b88b300a22d0ca5b161f
David Hull <david.hull37@gmail.com>
2020-06-05 17:14:03 -0500

    Have added Redis for session storage

--- app.js
+++ app.js
@@ -1,34 +1,70 @@
 require('dotenv').config();
+
 const bodyParser = require('body-parser');
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
+//  const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-// const enforce = require('express-sslify');
+const compression = require('compression');
+const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
+const session = require('express-session');
+const redis = require('redis');
+const RedisStore = require('connect-redis')(session);
 
 const app = express();
+const redisClient = redis.createClient({ url: process.env.REDIS_URL });
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('trust proxy', 1);
 
+app.use(cors());
+app.use(compression());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Enforces HTTPS
-// app.use(enforce.HTTPS({ trustProtoHeader: true }));
+if (process.env.NODE_ENV === 'production') {
+  // eslint-disable-next-line global-require
+  const enforce = require('express-sslify');
+
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
+}
+
+// Redis Set Up
+app.use(
+  session({
+    genid: () => {
+      return uuidv4();
+    },
+    store: new RedisStore({
+      client: redisClient
+    }),
+    name: '_redisGazorpian',
+    secret: process.env.REDIS_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true, maxAge: 120000 }
+  })
+);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+redisClient.on('error', (err) => {
+  console.log('Redis error:', err);
+});
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -36,11 +72,9 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res) => {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
